68438acbf7d6012ad5ce29780e522389613467f0
bazsarmartin <bazsarmartin1@gmail.com>
2023-04-23 19:38:09 +0200

    Added characterdetails activity, fragment and viewmodel

--- app/src/main/java/com/example/rick_and_morty_character_guide/activities/CharacterDetailsActivity.kt
+++ app/src/main/java/com/example/rick_and_morty_character_guide/activities/CharacterDetailsActivity.kt
@@ -1,4 +1,18 @@
 package com.example.rick_and_morty_character_guide.activities
 
-class CharacterDetailsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rick_and_morty_character_guide.R
+import com.example.rick_and_morty_character_guide.fragments.CharacterDetailsFragment
+
+class CharacterDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.character_details_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, CharacterDetailsFragment.newInstance())
+                .commitNow()
+        }
+    }
 }
